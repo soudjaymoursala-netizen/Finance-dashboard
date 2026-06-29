f1941fad972be406f26a40784b9a18a4e075b2b1
Mours <soudjaymoursala@gmail.com>
2026-06-29 10:27:36 +0200

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,18 @@
+const CONFIG = {
+
+    URL_BUDGET:
+        "https://docs.google.com/spreadsheets/d/REDACTED_SHEET_ID_BUDGET/export?format=csv&gid=519498006",
+
+    URL_EVOLUTION:
+        "https://docs.google.com/spreadsheets/d/REDACTED_SHEET_ID_BUDGET/export?format=csv&gid=810332816",
+
+    URL_OBJECTIF:
+        "https://docs.google.com/spreadsheets/d/REDACTED_SHEET_ID_BUDGET/export?format=csv&gid=1700667008",
+
+    URL_PEA:
+        "https://docs.google.com/spreadsheets/d/REDACTED_SHEET_ID_PEA/export?format=csv&gid=1971681206",
+
+    URL_CTO:
+        "https://docs.google.com/spreadsheets/d/REDACTED_SHEET_ID_CTO/export?format=csv&gid=1361663202"
+
+};
